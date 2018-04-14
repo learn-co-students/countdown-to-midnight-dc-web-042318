ebdca96ba38340c34e037b20deb48aebce8bdf5d
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-04-14 14:12:15 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(num_of_seconds)
+  while 0 < num_of_seconds
+  phrase = "#{num_of_seconds} SECOND(S)!"
+  puts phrase
+  num_of_seconds -= 1
+end
+return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num_of_seconds)
+  while 0 < num_of_seconds
+  sleep 1
+  phrase = "#{num_of_seconds} SECOND(S)!"
+  puts phrase
+  num_of_seconds -= 1
+end
+return "HAPPY NEW YEAR!"
 end
